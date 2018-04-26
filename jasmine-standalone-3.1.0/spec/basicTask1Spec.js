bfa9e58aceed4dc562f79beb8e62a2a05c2be370
matthiasliszt <mliszt.program@gmail.com>
2018-04-26 10:10:55 +0200

    test failed

---
+++ jasmine-standalone-3.1.0/spec/basicTask1Spec.js
@@ -0,0 +1,10 @@
+describe("basicTask1", function(){
+ 
+        // what it should do
+        it("it should return My favorite sandwich is a hamburger which has approximately 260 calories, ...",function(){
+                //expect something
+                expect(basicTask1()).toEqual('My favorite sandwich is a hamburger which has approximately 260 calories, along with it I enjoy eating Large French Fries which have about 570 calories.');
+        });
+ 
+});
+ 
---
+++ jasmine-standalone-3.1.0/src/basicTask1.js
@@ -0,0 +1,14 @@
+var sandwiches = { "sandwich": "hamburger", "calories": "260" };
+var fries = { "fries_size": "Large French Fries", "calories": "570" };
+
+//My favorite sandwich is a hamburger which has approximately 260 calories, 
+//along with it I enjoy eating Large French Fries which have about 570 calories.
+
+function basicTask1(){
+
+ var part1="My favorite sandwich is a "+sandwiches.sandwich+" which has approximately "+sandwiches.calories+ " calories, ";
+ var part2="along with it I enjoy eating "+fries.["fries_size"]+" which have about "+fries.calories+" calories.";
+ return part1+part2;
+}
+
+//document.write(basicTask1());
